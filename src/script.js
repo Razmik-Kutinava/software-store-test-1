0a591793dde65f1067de3d1e91b229c8b0842d5b
Razmik Kutinava <razmikg1988@gmail.com>
2025-03-22 21:09:25 +0400

    order

--- src/script.js
+++ src/script.js
@@ -24,14 +24,52 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-    updateCart();
+    if (document.getElementById('cart-items')) {
+        updateCart();
+    }
 });
 
 function addToCart(productName, price) {
-    const cart = JSON.parse(localStorage.getItem('cart')) || [];
-    cart.push({ productName, price });
-    localStorage.setItem('cart', JSON.stringify(cart));
-    window.location.href = 'cart.html';
+    const orderId = generateOrderId();
+    const licenseDuration = document.querySelector(`#${productName.toLowerCase().replace(/ /g, '-')}-license-value`).textContent;
+    const version = document.querySelector(`#${productName.toLowerCase().replace(/ /g, '-')}-version`).value;
+    const support = document.querySelector(`input[name="${productName.toLowerCase().replace(/ /g, '-')}-support"]`).checked ? 'Yes' : 'No';
+    const training = document.querySelector(`input[name="${productName.toLowerCase().replace(/ /g, '-')}-training"]`).checked ? 'Yes' : 'No';
+    const comments = document.querySelector(`#${productName.toLowerCase().replace(/ /g, '-')}-comments`).value;
+
+    const data = {
+        orderId,
+        productName,
+        price,
+        licenseDuration,
+        version,
+        support,
+        training,
+        comments
+    };
+
+    fetch('https://script.google.com/macros/s/AKfycbzzw1JmyZ7EGboaZ_Q22-_q-ChJr30BT5VRzaMJ0A_CAFZUUyCUL_bxgWcOzygeXspz/exec', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(result => {
+        if (result.status === 'success') {
+            const cart = JSON.parse(localStorage.getItem('cart')) || [];
+            cart.push(data);
+            localStorage.setItem('cart', JSON.stringify(cart));
+            window.location.href = 'cart.html';
+        } else {
+            alert('Ошибка при добавлении товара в корзину.');
+        }
+    })
+    .catch(error => {
+        console.error('Ошибка:', error);
+        alert('Ошибка при добавлении товара в корзину.');
+    });
 }
 
 function updateCart() {
@@ -54,4 +92,8 @@ function updateCart() {
 function proceedToCheckout() {
     alert('Переход к оплате...');
     // Здесь можно добавить логику для перехода к оплате
+}
+
+function generateOrderId() {
+    return 'ORD-' + Math.random().toString(36).substr(2, 9).toUpperCase();
 }
