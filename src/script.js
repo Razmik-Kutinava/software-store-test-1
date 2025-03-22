cb39f9717d95c2b00c6529785d3e56699ddfb19a
Razmik Kutinava <razmikg1988@gmail.com>
2025-03-22 20:45:18 +0400

    add area

--- src/script.js
+++ src/script.js
@@ -1,4 +1,26 @@
 /* filepath: c:\Tools\workarea\software-store\src\js\scripts.js */
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('JavaScript загружен и готов к работе');
+    const licenseSliders = document.querySelectorAll('.license-slider');
+
+    licenseSliders.forEach(slider => {
+        slider.addEventListener('input', (event) => {
+            const valueSpan = document.getElementById(`${event.target.id}-value`);
+            const value = event.target.value;
+            let text = '';
+
+            switch (value) {
+                case '1':
+                    text = '1 год';
+                    break;
+                case '2':
+                    text = '3 года';
+                    break;
+                case '3':
+                    text = '5 лет';
+                    break;
+            }
+
+            valueSpan.textContent = text;
+        });
+    });
 });
