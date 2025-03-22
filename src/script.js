931978e7e0d5455f60c1d6cfd13c0389f3748fd0
Razmik Kutinava <razmikg1988@gmail.com>
2025-03-22 19:09:05 +0400

    Initial new

---
+++ src/script.js
@@ -0,0 +1,4 @@
+/* filepath: c:\Tools\workarea\software-store\src\js\scripts.js */
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('JavaScript загружен и готов к работе');
+});
